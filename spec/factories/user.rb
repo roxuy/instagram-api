025389d35d749634d5f29b187a1fb8a925e7b18e
Rocio Godoy <rgodoylarrosa@gmail.com>
2023-01-13 02:52:46 -0300

    Add request-sing_in spec, fix user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     email      { Faker::Internet.unique.email }
     password { 'password' }
     password_confirmation { 'password' }
+    confirmed_at { Time.zone.today }
     fullname { Faker::Name.unique.name }
     username { Faker::Internet.unique.username }
   end
---
+++ spec/request/sign_in_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'POST /users/sign_in', type: :request do
+  subject { post user_session_path, params:, as: :json }
+
+  let(:user) { create(:user) }
+  let(:params) do
+    {
+      email: user.email,
+      password: user.password
+    }
+  end
+
+  context 'with correct params' do
+    before do
+      subject
+    end
+
+    it 'returns successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns access-token and client in the header of the response' do
+      expect(response.has_header?('access-token')).to eq(true)
+      expect(response.has_header?('client')).to eq(true)
+    end
+  end
+
+  context 'with invalid params' do
+    let(:params) do
+      {
+        email: user.email,
+        password: 'notmypassword'
+      }
+    end
+
+    before do
+      subject
+    end
+
+    it 'returns unauthorized response' do
+      expect(response).to be_unauthorized
+    end
+
+    it 'shouldnt return access-token and client in the header of the response' do
+      expect(response.has_header?('access-token')).to eq(false)
+      expect(response.has_header?('client')).to eq(false)
+    end
+
+    it 'returns a message error' do
+      json = JSON.parse(response.body)
+      expect(json['errors']).to include('Invalid login credentials. Please try again.')
+    end
+  end
+end
